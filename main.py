a8499f8b90dd7e9691e1ff2e29833e45891e20fd
Sharaar <walidsharar@gmail.com>
2021-06-22 17:29:34 +0200

    move snake

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import  Screen , Turtle
 
 screen = Screen()
@@ -5,12 +6,32 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game!")
+#Turn turtle animation on/off and set delay for update drawings.
+screen.tracer(0)
 
 start_position =[(0,0),(-20,0),(-40,0)]
+parts=[]
 for position in start_position:
     part = Turtle("square")
     part.color("white")
+    part.penup()
     part.goto(position)
+    parts.append(part)
+
+
+game_is_on=True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for part_num in range (len(parts)-1,0,-1):
+        new_x = parts[part_num-1].xcor()
+        new_y=parts [part_num-1].ycor()
+        parts[part_num].goto(new_x,new_y)
+    parts[0].forward(20)
+
+
+
 
 
 
