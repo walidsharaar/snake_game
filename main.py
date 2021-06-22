857d4cb56adbcf84f996c4337d3a1fc5e582a4d1
Sharaar <walidsharar@gmail.com>
2021-06-22 17:06:34 +0200

    Sanke position created

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game!")
 
+start_position =[(0,0),(-20,0),(-40,0)]
+for position in start_position:
+    part = Turtle("square")
+    part.color("white")
+    part.goto(position)
+
 
 
 screen.exitonclick()
